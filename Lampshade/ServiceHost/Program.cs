c028a200b4907e95d907ce16a4920d42a9b2056c
Arsalan Khodadadi <arsalan.sc30@gmail.com>
2023-07-22 16:22:25 +0330

    ProductCategory Management Implemented

---
+++ Lampshade/ServiceHost/Program.cs
@@ -0,0 +1,29 @@
+using ShopManagement.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("LampshadeDb");
+ShopManagementBootstrapper.Configure(builder.Services, connectionString);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
